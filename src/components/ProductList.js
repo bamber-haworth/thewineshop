def9e6edb8fdc145b7036e9d73569f800895538e
Beth <bethhaworth21@gmail.com>
2018-12-30 18:13:31 +0000

    Set internal state

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,16 +3,20 @@ import './ProductList.css';
 import Product from './Product';
 
 class ProductList extends React.Component {
+   constructor () {
+       super();
+       this.state = { products: [] };
+       fetch("products.json")
+        .then(response => response.json())
+        .then(json => (this.setState({products: json})))
+        .catch(error => console.log(error));
+   }
+   
     render() {
-        let products = [
-            { code:"P01", name: "Traditional Merlot", description: "A bottle of middle weight wine, lower in tannins (smoother), with a more red-fruited flavour profile."},
-            { code: "P02", name: "Classic Chianti", description: "A medium bodied wine characterised by a marvellous freshness with a lingering, fruity finish."},
-            { code: "P03", name: "Chardonnay", description: " A dry, full-bodied white wine with spicy, bourbony notes in an elegant bottle."},
-        ];
         
         let productComponents = [];
 
-        for (let product of products) {
+        for (let product of this.state.products) {
             productComponents.push(<Product item={product}/>);
         }
 
